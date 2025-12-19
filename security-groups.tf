b82fa9c519ccf91fae7b7fd3cd39c3807c7b88a6
Mohamed-Sarhan-Elhussieny <167382376+Mohamed-Sarhan-Elhussieny@users.noreply.github.com>
2025-12-20 00:18:56 +0200

    Update security group with additional ingress rules
    Added ingress rules for custom ports and services.

--- security-groups.tf
+++ security-groups.tf
@@ -1,13 +1,11 @@
-
-
-# #************************* aws_security_group ****************************************#
+# ************************* Security Groups ****************************************#
 
 resource "aws_security_group" "security_group1" {
   name        = "security-group(80,443,8080,22)"
   description = "Allow inbound traffic on ports 22, 80, 443, 8080"
   vpc_id      = aws_vpc.vpc_depi.id
 
-  # Inbound rules
+  # SSH
   ingress {
     from_port   = 22
     to_port     = 22
@@ -15,6 +13,7 @@ resource "aws_security_group" "security_group1" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # HTTP
   ingress {
     from_port   = 80
     to_port     = 80
@@ -22,28 +21,42 @@ resource "aws_security_group" "security_group1" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # HTTPS
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-    ingress {
+
+  # Custom port 32000
+  ingress {
     from_port   = 32000
     to_port     = 32000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-    ingress {
+
+  # Jenkins (50000)
+  ingress {
     from_port   = 50000
     to_port     = 50000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Grafana (3000)
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Prometheus (9090)
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -60,4 +73,3 @@ resource "aws_security_group" "security_group1" {
     Name = "security-group1"
   }
 }
-
